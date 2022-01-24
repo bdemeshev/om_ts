d03caddcb84ce4fa916f4fddab074fb6d96df646
bdemeshev <boris.demeshev@gmail.com>
2022-01-24 11:12:57 +0300

    lec 3

--- lec_03/code/plots_03.R
+++ lec_03/code/plots_03.R
@@ -28,6 +28,24 @@ marriages = mutate(marriages, total = total / 1000)
 marr_rf = filter(marriages, code == 643)
 
 
+# 3-19
+library(rvest)
+url = 'http://sophist.hse.ru/hse/1/tables/POPNUM_Y.htm'
+xml_tree = read_html(url)
+p1 = html_table(xml_tree)[[1]]
+colnames(p1) = c('year', 'total')
+p2 = tail(p1, -2)
+p3 = head(p2, -4)
+p4 = mutate(p3, year = as.numeric(year), total = as.numeric(total) / 1000)
+popnum = as_tsibble(p4, index = 'year')
+autoplot(popnum, total)
+
+mods = model(popnum,
+        aadn = ETS(total ~ error('A') + trend('Ad') + season('N')))
+fcst = forecast(mods, h = '12 years')
+autoplot(fcst, popnum) + theme_light() + labs(x = NULL, y = 'Население, млн. человек', title = 'ETS(AAdN): прогноз численности населения России')
+ggsave("../pictures/om_ts_03-019.png", dpi = 300, width = plot_width, height = plot_height, unit = 'cm')
+
 
 # 3-33
 aus = filter(aus_production, Quarter < yearquarter('1981Q1'))
@@ -36,6 +54,43 @@ autoplot(aus, Electricity/1000) +
        x = '', y = 'Энергия, терават-часы') +
   theme_light()
 ggsave("../pictures/om_ts_03-033.png", dpi = 300, width = plot_width, height = plot_height, unit = 'cm')
+
+
+# 3-47
+
+aus = filter(aus_production, Quarter < yearquarter('1981Q1'))
+aus = mutate(aus, Electricity = Electricity / 1000)
+aus_left = filter(aus, Quarter < yearquarter('1968Q1'))
+m_left = model(aus_left,
+               mnm = ETS(Electricity ~ error('M') + trend('N') + season('M')))
+m_full = model(aus,
+               mnm = ETS(Electricity ~ error('M') + trend('N') + season('M')))
+f_left = forecast(m_left, h = '5 years')
+f_full = forecast(m_full, h = '5 years')
+
+autoplot(f_left, aus)
+
+autoplot(f_full, aus) + autoplot(f_left, aus)
+f_left
+f_full
+
+f_mid = tibble(.model = 'mnm', Electricity = NA, .mean = NA,
+               Quarter = yearquarter(ymd('1973-1-1') + months(seq(0, 83, by = 3))))
+
+
+f_join = bind_rows(as_tsibble(f_left), f_mid, as_tibble(f_full))
+f_join = as_tsibble(f_join, index = Quarter, key = .model)
+f_join = as_fable(f_join, distribution = 'Electricity', response = 'Electricity')
+f_join
+
+
+autoplot(f_join, aus) +
+  labs(title = 'ETS(MNM): производство электроэнергии в Австралии',
+       x = '', y = 'Энергия, терават-часы') +
+  theme_light() + theme(legend.position = "none")
+
+ggsave("../pictures/om_ts_03-047.png", dpi = 300, width = plot_width, height = plot_height, unit = 'cm')
+
 
 
 # 3-60
